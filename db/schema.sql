620efcc57b8908f2bcc83fc7c4e161d3a8010721
Hlaing Aung <betahba@gmail.com>
2021-12-13 16:55:40 -0500

    added foreign keys for tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS employeeRole;
+DROP TABLE IF EXISTS department;
+
+
+
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
@@ -7,7 +13,8 @@ CREATE TABLE employeeRole (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(5,2),
-  department_id INTEGER
+  department_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -15,5 +22,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  manager_id INTEGER
+  manager_id INTEGER,
+  CONSTRAINT fk_roleId FOREIGN KEY (role_id) REFERENCES employeeRole(id) ON DELETE SET NULL,
+  CONSTRAINT fk_managerId FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,18 +6,18 @@ VALUES
 
 INSERT INTO employeeRole (title, salary, department_id)
 VALUES
-  ('salesManager', 100.00, 2),
-  ('hrManager', 150.00, 3),
-  ('engineersManager', 200.00, 1),
-  ('sales', 50.00, 2),
-  ('hr', 75.00, 3),
-  ('engineers', 100.00, 1);
+  ('salesManager', 100.00, 1),
+  ('hrManager', 150.00, 2),
+  ('engineersManager', 200.00, 3),
+  ('sales', 50.00, 1),
+  ('hr', 75.00, 2),
+  ('engineers', 100.00, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('bob', 'mick', 20, NULL),
-  ('mark', 'jacobs', 15, NULL),
-  ('james', 'mac', 2, NULL),
-  ('tim', 'tat', 52, 20),
-  ('tom', 'fae', 73, 15),
-  ('max', 'crow', 10, 2);
+  ('bob', 'mick', 1, NULL),
+  ('mark', 'jacobs', 2, NULL),
+  ('james', 'mac', 3, NULL),
+  ('tim', 'tat', 4, 20),
+  ('tom', 'fae', 5, 15),
+  ('max', 'crow', 6, 2);
